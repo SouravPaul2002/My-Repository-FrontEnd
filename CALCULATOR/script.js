821ab109585d93f9e3c859ba8d096809807ccf20
SouravPaul2002 <sourav97972@gmail.com>
2023-10-18 11:13:54 +0530

    js added 30%

--- CALCULATOR/script.js
+++ CALCULATOR/script.js
@@ -1,7 +1,7 @@
 let string = "";
 let num = document.querySelectorAll(".number");
-Array.from(num).forEach((num)=>{
-    num.addEventListener('click', (a)=>{
+Array.from(num).forEach((num) => {
+    num.addEventListener('click', (a) => {
         console.log(a.target);
         string = string + a.target.innerHTML;
         document.querySelector("input").value = string;
